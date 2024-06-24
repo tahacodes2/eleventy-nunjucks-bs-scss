debea9f0376d78e94e0b7157923e5cbb67af013e
tahacodes2 <ework.tdx@gmail.com>
2024-06-24 12:59:42 +0100

    old code why not

---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Content files referencing CSS classes
+    content: ["./dist/**/*.html" , "./dist/**/*.js"],
+  
+    // CSS files to be purged in-place
+    css: ["./dist/css/*.css"],
+  };
